acc14dfa99154bbcc61693a174bb63bf8884596b
FRLOfficial <50067432+FRLOfficial@users.noreply.github.com>
2019-04-28 13:07:20 +0700

    Add files via upload

---
+++ Pulsa.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+#Lumayan Buat Pamer KeKwan Lu😂
+#Tools Ini Cuman Buat Iseng² doang
+#Dilarang Recode Sebelum Ijin FRL Official
+ 
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 3
+clear
+figlet "FRL_ID"|lolcat
+echo ""
+echo $cyan "Created by FRL Official"
+echo $cyan "WA : 085738995***"
+echo $cyan "FB : FRL_ID"
+echo $cyan "YT : FRL Official"
+echo $cyan "IG : @frl.23"
+echo $cyan "Notif : Gunakan Dengan Bijak"
+echo $yellow "All Operator Pulsa,Jadi Gunakan Dengan Bijak"
+echo $green "Jika Ada Apa Apa Gw Gk Tanggung Jawab Kan Udh Gw Kasih Tau Gunakan Dengan Bijak"
+sleep 5
+echo $white "Waiting ........."
+sleep 10
+echo $cyan " Masukkan Kartu Simcard Anda :"
+read kartu
+echo $cyan " Massukkan Nomor Anda :"
+read anda
+echo "\033[0;32m]Jumlah Pulsa : "
+read pulsa
+echo $red "Loading ....."
+sleep 5
+echo $red "Waiting......."
+sleep 5
+echo $red "Sedang MengCrack Pulsa $kartu"
+echo $red "Booting system......"
+sleep 5
+echo $red "Sedang Memasukkan Pulsa Ke $anda"
+sleep 3
+echo $red "Cracking Pulsa ......"
+sleep 5
+echo $red "Berjumlah $pulsa Sedang Mengirim......"
+sleep 5
+echo "Loading...."
+sleep 3
+echo "1%"
+sleep 3
+echo "35%"
+sleep 3
+echo "50%"
+echo "60%"
+sleep 3
+echo "80%"
+sleep 3
+echo "90%"
+sleep 3
+echo "100%"
+sleep 5
+echo "Waiting........."
+sleep 1
+echo "Done......"
+sleep 1
+echo "Kartu Anda : $kartu"
+echo "Sukes Mengirim Ke : $anda"
+echo "Berjumlah : $pulsa"
+echo "Terima Kasih ^_^"
+echo "Jangan Lupa Subscribe Chanel FRL Official"
